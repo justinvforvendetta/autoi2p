7e6665b671645d7ce7b1d5602b88c94df0c1e35d
blackwat3r <blackwat3r@users.noreply.github.com>
2015-02-18 20:39:49 -0500

    fix config copying
    fix config copying test

--- autodogedi2p.sh
+++ autodogedi2p.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-#todo_setup_much_configs
 path=$(pwd)
+
+function dog_conf () {
+    mkdir -p $HOME/.DogeCoinDark
+    chmod -R 0777 $HOME/.DogeCoinDark
+    cp $path/DogeCoinDark.conf $HOME/.DogeCoinDark/DogeCoinDark.conf
+}
+
+
+function i2p_conf () {
+
+    cp $path/i2ptunnel.config /usr/share/i2p/i2ptunnel.config
+}
+
 if [[ $EUID -eq 0 ]] 
 then
     apt-get install -f --force-yes --yes build-essential pkg-config libtool autotools-dev autoconf automake libssl-dev libboost-all-dev libdb5.3-dev  libdb5.3++-dev libminiupnpc-dev libdb++-dev qt4-qmake libqt4-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libminiupnpc8 git 
@@ -17,24 +29,12 @@ then
     then
      cp dogecoindark-qt /usr/bin/dogecoindark-qt
     fi
-    mkdir -p $HOME/.DogeCoinDark
-    chmod -R 0777 $HOME/.DogeCoinDark
-    if [[ -f $HOME/.DogeCoinDark/DogeCoinDark.conf ]]
-    then 
-     mv $HOME/.DogeCoinDark/DogeCoinDark.conf $HOME/.DogeCoinDark/DogeCoinDark.conf.bak
-     cp $path/DogeCoinDark.conf >> $HOME/.DogeCoinDark/DogeCoinDark.conf
-    fi
-  
+    dog_conf
     apt-add-repository ppa:i2p-maintainers/i2p -y
     apt-get update
     apt-get install i2p default-jre -y
     dpkg-reconfigure i2p
-    if [[ -f /usr/share/i2p/i2ptunnel.config ]] 
-    then
-    mv /usr/share/i2p/i2ptunnel.config /usr/share/i2p/i2ptunnel.config.bak
-    cp $path/i2ptunnel.config /usr/share/i2p/i2ptunnel.config
-    fi
-    
+    i2p_conf
     sed -i s/RUN_DAEMON=\"false\"/RUN_DAEMON=\"true\"/ /etc/default/i2p
     service i2p start
     #/etc/init.d/i2p start
